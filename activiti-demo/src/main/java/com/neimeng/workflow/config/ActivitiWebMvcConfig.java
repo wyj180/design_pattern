691b0e7d87af8cd8dbec74d276b066f268f3079b
wangyj <l>
2019-11-03 23:03:44 +0800

    spring boot2.1.6整合Activiti6.0，添加在线流程设计器activiti modoler

---
+++ activiti-demo/src/main/java/com/neimeng/workflow/config/ActivitiWebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.neimeng.workflow.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@Slf4j
+public class ActivitiWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
+    }
+
+    /* 设置访问根路径"/"时，访问index.ftl */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}
